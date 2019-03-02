36afae0da75f2a6532b3e0d2c641b0ef420d2c8c
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2019-03-02 10:45:59 +0100

    Pico: try to enable PMU watchdog.
    THIS SEEMS TO CASE THE USB TO DISCONNECT AFTER A FEW SECONDOS!

--- Pico SP-H03/tccvirus.s
+++ Pico SP-H03/tccvirus.s
@@ -8,6 +8,9 @@
 	.type	main, %function
 .set  EN, (1<<0)
 .set IEN, (1<<3)
+.set TCKSEL, (1<<4)
+.set WATCHDOG_EN, (1<<31)
+
 main:
 	ldr	r3, .TWDCFG
 	ldrb	r4, [r3]
@@ -17,7 +20,13 @@ main:
 	bic	r4, r4, 0x30 @ clear the TCKSEL
 	orr	r4, r4, 0x40 @ set the TCKSEL to 0x04
 	strb	r4, [r3]
+	ldr	r3, .WATCHDOG_ADDR
+	ldr	r4, [r3]
+	orr	r4, r4, #WATCHDOG_EN
+	str	r4, [r3]
 .loop:
 	b .loop
 .TWDCFG:
 	.word	0xf0403070
+.WATCHDOG_ADDR:
+	.word	0xf040400c
