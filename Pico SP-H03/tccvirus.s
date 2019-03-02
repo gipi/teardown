cec2909c919d2dbf6a57a74229856db45636434b
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2019-03-02 10:44:55 +0100

    Pico: make code more readable.

--- Pico SP-H03/tccvirus.s
+++ Pico SP-H03/tccvirus.s
@@ -6,16 +6,18 @@
 	.arm
 	.fpu softvfp
 	.type	main, %function
+.set  EN, (1<<0)
+.set IEN, (1<<3)
 main:
-	ldr	r3, .L3
+	ldr	r3, .TWDCFG
 	ldrb	r4, [r3]
-	orr	r4, r4, #1
+	orr	r4, r4, #(EN | IEN)
 	strb	r4, [r3]
 	ldrb	r4, [r3]
-	bic	r4, r4, #48
-	orr	r4, r4, #64
+	bic	r4, r4, 0x30 @ clear the TCKSEL
+	orr	r4, r4, 0x40 @ set the TCKSEL to 0x04
 	strb	r4, [r3]
 .loop:
 	b .loop
-.L3:
+.TWDCFG:
 	.word	0xf0403070
