71a9c85226ffb34b3b2331dae9020bb1fa4ac672
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2019-04-09 23:15:13 +0200

    serial_upload: rework and add a progress bar.
    Now it logs the communications in a separate file.

--- serial_upload.py
+++ serial_upload.py
@@ -2,15 +2,32 @@
 '''
 Upload a file from a serial connection.
 '''
+import os
 import sys
 import time
 import serial
 import logging
+import tqdm # for the progress bar
 
 
-logging.basicConfig()
-logger = logging.getLogger(__name__)
-logger.setLevel('DEBUG')
+# we want to log on file the communications
+# and in console the INFO
+#logging.basicConfig()
+logger_console = logging.getLogger('%s.info' % __name__)
+logger_console.setLevel(logging.INFO)
+stream = logging.StreamHandler()
+logger_console.addHandler(stream)
+
+
+log_comm = logging.getLogger('%s.comm' % __name__)
+log_comm.setLevel(logging.DEBUG)
+# create a file handler
+handler = logging.FileHandler('serial_upload.log')
+handler.setLevel(logging.DEBUG)
+
+logger_console.addHandler(handler)
+log_comm.addHandler(handler)
+
 
 
 
@@ -29,7 +46,7 @@ def serial_shell_cmd(s, cmd, one_more_line=False):
     '''
     marker = b'RandomJunk'
     line = b"%s; echo -ne %s\r\n" % (cmd, marker)
-    logger.debug('> %s' % line)
+    log_comm.debug('> %s' % line)
     s.write(line)
 
     string_end = b"%s# " % marker
@@ -37,14 +54,14 @@ def serial_shell_cmd(s, cmd, one_more_line=False):
     #line_received = s.readline()
     #logger.debug('< %s' % line_received)
     line_received = s.read_until(string_end)
-    logger.debug('< %s' % line_received)
+    log_comm.debug('< %s' % line_received)
 
     if line not in line_received:
         #print(s.readline())
         #raise AttributeError('received:%s vs sent:%s' % (line_received, line))
         pass
     line_received = line_received[:-len(string_end)]
-    logger.debug('< %s' % line_received)
+    log_comm.debug('< %s' % line_received)
     return line_received
 
 
@@ -52,20 +69,30 @@ if __name__ == '__main__':
     serial_port_path = sys.argv[1]
     serial_baud = sys.argv[2]
     local_path  = sys.argv[3]
-    remote_path = bytes(sys.argv[4], 'utf-8')
+    remote_path = sys.argv[4]
 
+    logger_console.info(' [*] Opening %s port' % serial_port_path)
     connection = serial.Serial(serial_port_path, serial_baud)
     #connection.timeout = .4
 
+    logger_console.info(' [*] Creating empty file %s' % remote_path)
+    remote_path = bytes(sys.argv[4], 'utf-8')
     serial_shell_cmd(connection, b'echo -ne "" > \'%s\'' % remote_path)
 
-    with open(local_path, "rb") as output:
+    CHUNK_SIZE = 64
+    local_file_size = os.stat(local_path).st_size
+
+    logger_console.info(' [*] Starting to upload file %s (size %d)' % (local_path, local_file_size))
+
+    with open(local_path, "rb") as output, tqdm.tqdm(total=local_file_size) as progress:
         while True:
-            chunk = output.read(64)
-            if chunk == "":
+            chunk = output.read(CHUNK_SIZE)
+            if chunk == b"":
                 break
             encoded = get_encoded_repr(chunk)
             serial_shell_cmd(connection, b"echo -en '%s' >> '%s'" % (
                 bytes(encoded, 'utf-8'),
                 remote_path
             ), True)
+            progress.update(CHUNK_SIZE)
+
