cca64e0a3f858b124567c8bb37152ac5e82d53a8
Gianluca Pacchiella <gp@ktln2.org>
2021-08-06 11:24:09 +0200

    open-adec: add minimal assert functionality.
    For now the line number is not available, since I would prefer waiting
    on implementing a sprintf that showing an hexadecimal number as new line.

---
+++ powkiddy/open-adec/assert.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2021 Gianluca Pacchiella <gp@ktln2.org>
+ *
+ * This file is part of open-adec.
+ *
+ * open-adec is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * open-adec is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with open-adec.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef __ASSERT_H__
+#define __ASSERT_H__
+#include "regs_io.h"
+
+
+
+#define ASSERT(expr) \
+  do {                                          \
+    if (!(expr)) {                              \
+      _assert(__FILE__, __LINE__, #expr);       \
+    }                                           \
+  } while (0)
+
+
+void _assert(const char* filename, u32 linenum, const char* expr);
+
+#endif
