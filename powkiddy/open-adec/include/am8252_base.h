2f7835948d84378e09a7885e000f4e052a4f0425
Gianluca Pacchiella <gp@ktln2.org>
2021-08-12 15:04:07 +0200

    open-adec: reorganize source files.

---
+++ powkiddy/open-adec/include/am8252_base.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2021 Gianluca Pacchiella <gp@ktln2.org>
+ *
+ * This file is part of open-adec.
+ *
+ * open-adec is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * open-adec is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with open-adec.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef __REGS_BASE_H__
+#define __REGS_BASE_H__
+
+
+#define UART_BASE      0xb0190020
+#define OTG_BASE       0xb00e0000
+
+
+#endif
---
+++ powkiddy/open-adec/include/ats3603_base.h
@@ -0,0 +1,9 @@
+#ifndef __REGS_BASE_H__
+#define __REGS_BASE_H__
+
+
+#define UART_BASE      0xb0168000
+#define OTG_BASE       0xb0290000
+
+
+#endif
